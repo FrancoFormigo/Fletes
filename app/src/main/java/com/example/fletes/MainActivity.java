5a83a1b89ab1a31120f901887dd5d38053f3fbe6
FrancoFormigo <francoformigo@hotmail.com>
2022-11-02 23:20:01 -0300

    primera carga de appFletes

--- app/src/main/java/com/example/fletes/MainActivity.java
+++ app/src/main/java/com/example/fletes/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.fletes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fletes.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activarBotones();
+    }
+
+    private void activarBotones() {
+        Button btSalir= findViewById(R.id.btSalir);
+        btSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accionBotonSalir();
+            }
+        });
+
+        Button btFletero= findViewById(R.id.btFletero);
+        btFletero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accionBotonFletero();
+            }
+        });
+
+        Button btCliente= findViewById(R.id.btCliente);
+        btCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accionBotonCliente();
+            }
+        });
+    }
+
+    private void accionBotonCliente() {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        navigationActivity.putExtra("Cliente","No registrado");
+        startActivity(loginActivity);
+    }
+
+    private void accionBotonFletero() {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
+    }
+
+    private void accionBotonSalir() {
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/fletes/NavigationActivity.java
@@ -0,0 +1,80 @@
+package com.example.fletes;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fletes.databinding.ActivityNavigationBinding;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityNavigationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getIntent().getExtras();
+        String usuario = bundle.getString("Cliente");
+
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarNavigation.toolbar);
+        binding.appBarNavigation.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        Menu miMenu= navigationView.getMenu();
+        if (usuario.equals("No registrado")){
+            miMenu.findItem(R.id.nav_mis_pedidos).setEnabled(true);
+            miMenu.findItem(R.id.nav_crear_pedido).setEnabled(true);
+            miMenu.findItem(R.id.nav_ver_pedidos).setEnabled(false);
+            miMenu.findItem(R.id.nav_mis_fletes).setEnabled(false);
+        }else{
+            miMenu.findItem(R.id.nav_mis_pedidos).setEnabled(false);
+            miMenu.findItem(R.id.nav_crear_pedido).setEnabled(false);
+            miMenu.findItem(R.id.nav_ver_pedidos).setEnabled(true);
+            miMenu.findItem(R.id.nav_mis_fletes).setEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
